c734672b0ad13ba3c87c4b15b41f8fc0004dff39
Aguiar575 <aguiarus99arthurus@gmail.com>
2024-04-23 23:51:10 -0300

    configuring dotenv and mysql adapter

--- internal/db.go
+++ internal/db.go
@@ -1,19 +1,25 @@
 package db
 
 import (
-  "database/sql"
-  "fmt"
-  _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func ConnectToDB(host string, port string, username string, password string) (*sql.DB, error) {
-    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/?parseTime=true", username, password, host, port)
-    db, err := sql.Open("mysql", dsn)
-    if err != nil {
-        return nil, fmt.Errorf("failed to connect to database: %w", err)
-    }
-    if err := db.Ping(); err != nil {
-        return nil, fmt.Errorf("failed to ping database: %w", err)
-    }
-    return db, nil
+func ConnectToDB(
+	host string, port string,
+	username string, password string) (*sql.DB, error) {
+	var dsn string = 
+    fmt.Sprintf("%s:%s@tcp(%s:%s)/?parseTime=true", username, password, host, port)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	return db, nil
 }
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"os"
+	db "user-management/internal"
+)
 
 func main() {
+  err := godotenv.Load()
+  if err != nil {
+    fmt.Println("Error loading .env file")
+  }
+
+	db, err := db.ConnectToDB(os.Getenv("HOST"), os.Getenv("PORT"),
+		os.Getenv("USERNAME"), os.Getenv("PASSWORD"))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	fmt.Println("Hello, World!")
 }
